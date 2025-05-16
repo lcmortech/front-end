59c1c0d6b26cc4a8b3a1a6bb86649ee0a41e11a7
Ren <rg326@live.com>
2025-05-15 22:21:54 -0400

    added linkedin react code

---
+++ code-tutorials/li-learn/react-project/src/App.jsx
@@ -0,0 +1,42 @@
+import "./App.css"
+
+let lang = "React";
+let atom = "⚛️";
+
+const items = [
+  "Mac and Cheese",
+  "California Rolls",
+  "Taco Salad",
+  "Tofu Stir Fry"
+]
+
+const dishObjs = items.map(dish, i => ({id: i, title: dish}))
+
+function Main({dishes}){
+  return(
+    <ul>
+      {dishes.map(dish => <li key={dish.id} style={{listStyleType: "none"}}>{dish.title}</li>)}
+    </ul>
+  )
+}
+
+function Header({name, year}){
+  return(
+  <>
+    <h1>{name}'s Kitchen</h1>
+    <p>Copyright {year}</p>
+  </>
+  )
+}
+
+export default function App() {
+  return(
+    <>
+      <Header name="Alex" year={new Date().getFullYear()}/>
+      <Main dishes={dishObjs}/>
+        <h6>
+          {`Hello, from ${lang} ${atom}`}!!    
+        </h6>
+    </>
+  )
+}
