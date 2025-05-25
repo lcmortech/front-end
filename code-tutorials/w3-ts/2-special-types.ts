b3132114bccf292dd78e64426879585e4bd7cc1f
Lauren <rg326@live.com>
2025-05-24 23:13:22 -0400

    added “unknown” type to 2-special-types.ts

--- code-tutorials/w3-ts/2-special-types.ts
+++ code-tutorials/w3-ts/2-special-types.ts
@@ -7,3 +7,12 @@ v = 'string' //no error as it can be "any" type
 Math.rounc(v); //no error as it can be "any" type
 
 //any can be a useful way to get past errors since it disables type checking, but TypeScript will not be able provide type safety, and tools which rely on type data, such as auto completion, will not work. Remember, it should be avoided at "any" cost...
+
+//Type: Unknown
+//[unknown] is a similar, but safer alternative to [any].
+
+//Typescript will prevent unknown types from being used, as shown in the below example: (on site)
+
+//unknown is best used when you don't know the type of data being typed. To add a type later, you'll need to cast it.
+
+//Casting is when we use the "as" keyword to say property or variable is of the casted type.
