d4179c69b0061a1ad64e6509b9d63bd90f3ab65a
Lauren <rg326@live.com>
2025-05-27 22:41:12 -0400

    added notes to 3-arrays.ts

--- code-tutorials/w3-ts/3-arrays.ts
+++ code-tutorials/w3-ts/3-arrays.ts
@@ -1,1 +1,18 @@
-//TS has a special
+//TS has a special syntax for typing arrays
+
+const names: string[] = [];
+names.push("Dylan")
+
+//Readonly
+//[readonly] keyword can prevent arrays from being changed.
+
+const names: readonly string[] = ["Dylan"]
+names.push("Jack") //Throws an error, property push does not exist on type 'readonly string[]'
+
+//Type Inference
+//TS can infer the type of an array if it has values
+
+const numbers = [1,2,3] //inferred to type number[]
+numbers.push(4) //No error
+numbers.push("2")//Error
+let head: number = numbers[0] //No error
