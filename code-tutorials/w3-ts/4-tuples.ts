2e5e1ca63010b20cfd5eeeeff3a884a8f106b999
Lauren <rg326@live.com>
2025-05-29 23:27:43 -0400

    added notes and code on readonly tuples to 4-tuples.ts

--- code-tutorials/w3-ts/4-tuples.ts
+++ code-tutorials/w3-ts/4-tuples.ts
@@ -12,3 +12,20 @@ ourTuple = [5, false, 'Coding G was here'];
 let ourTuple: [number, boolean, string];
 //initialized incorrectly throws error
 ourTuple = [false, 'Coding G was mistaken', 5];
+
+//A good practice is to make your tuple [readonly]
+//Tuples only have strongly defined types for the initial values
+//define our tuple
+let ourTuple: [number, boolean, string];
+//initialize correctly
+ourTuple = [5, false, "Coding G was here"]
+//we have no type safety
+ourTuple.push('Something wrong')
+console.log(ourTuple);
+
+const ourReadOnlyTuple: readonly [number, boolean, string] = [5, true, "The Real Coding G"]
+//throws error
+ourReadOnlyTuple.push("Coding G took a day off")
+
+//Named Tuples
+//"Named tuples" Allow us to provide context for our values at each index
