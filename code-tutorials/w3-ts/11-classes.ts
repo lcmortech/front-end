a36bc7242c86725efcf03dac3801f71c78d84da4
Ren <rg326@live.com>
2025-07-04 23:32:20 -0400

    added examle code for abstract classes to 11-classes.ts

--- code-tutorials/w3-ts/11-classes.ts
+++ code-tutorials/w3-ts/11-classes.ts
@@ -144,4 +144,20 @@ class Square extends Rectangle {
 
 //Abstract Classes
 //Classes can be written in a way that allows them to be used as a base class for other classes without having to implement all the members. This is done by using the abstract keyword. Members that are left unimplemented also use the abstract keyword.
+abstract class Polygon {
+  public abstract getArea(): number;
 
+  public toString(): string {
+    return `Polygon[area=${this.getArea()}]`;
+  }
+}
+
+class Rectangle extends Polygon {
+  public constructor(protected readonly width: number, protected readonly height: number) {
+    super();
+  }
+
+  public getArea(): number {
+    return this.width * this.height;
+  }
+}
